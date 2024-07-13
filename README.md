64b8537471b3f5b2e3a5e844cc87499fec4da84f
Seanrutto <travisean.ts@gmail.com>
2024-07-13 14:03:44 +0300

    First question answers

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ALX_Simple_Quiz
+First js challenge
+It was not as easy as it looked at first
---
+++ quiz.js
@@ -0,0 +1,21 @@
+function checkAnswer(){
+    var correctAnswer = "4";
+    let userAnswerList = document.querySelector('input[name="quiz"]:checked');
+    let userAnswer = userAnswerList.value;
+
+    var feedbackElement = document.getElementById('feedback');
+
+    if(userAnswer === correctAnswer){
+        feedbackElement.textContent = "Correct! Well done.";
+    } else {
+        feedbackElement.textContent = "That's incorrect. Try again!";
+    }
+
+    
+
+
+}
+
+document.getElementById('submit-answer').addEventListener("click", checkAnswer);
+
+
